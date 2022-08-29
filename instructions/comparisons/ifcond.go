9106cb5c197f105151e2cfa96cc933d3c8b2228a
impact-eintr <yixingwei4@gmail.com>
2022-08-30 07:30:12 +0800

    ccomparisons instructions

---
+++ instructions/comparisons/ifcond.go
@@ -0,0 +1,74 @@
+package comparisons
+
+import (
+	"jvm/instructions/base"
+	"jvm/rtda"
+)
+
+// if<cond> 指令操作数栈顶的int变量弹出 然后跟0进行比较 满足条件则跳转
+
+type IFEQ struct {
+	base.BranchInstruction
+}
+
+func (self *IFEQ) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val == 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFNE struct {
+	base.BranchInstruction
+}
+
+func (self *IFNE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val != 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFLT struct {
+	base.BranchInstruction
+}
+
+func (self *IFLT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val < 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFLE struct {
+	base.BranchInstruction
+}
+
+func (self *IFLE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val <= 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFGT struct {
+	base.BranchInstruction
+}
+
+func (self *IFGT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val > 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFGE struct {
+	base.BranchInstruction
+}
+
+func (self *IFGE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val >= 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
