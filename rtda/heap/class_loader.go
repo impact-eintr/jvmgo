a7a9dc8bb887917612d84552b2b86a3ad18103bc
impact-eintr <yixingwei4@gmail.com>
2022-09-08 07:46:29 +0800

    class loader

--- rtda/heap/class_loader.go
+++ rtda/heap/class_loader.go
@@ -1,11 +1,14 @@
 package heap
 
 import (
+	"fmt"
+	"jvm/classfile"
 	"jvm/classpath"
 )
 
+// 类加载器
 type ClassLoader struct {
-	cp *classpath.Classpath
+	cp       *classpath.Classpath
 	classMap map[string]*Class
 }
 
@@ -13,9 +16,67 @@ type ClassLoader struct {
 // 2 解析 class 文件 生成虚拟机可以使用的类数据 并放入方法区
 // 3 进行链接
 
+// 构造函数
 func NewClassLoader(cp *classpath.Classpath) *ClassLoader {
 	return &ClassLoader{
-		cp: cp,
+		cp:       cp,
 		classMap: make(map[string]*Class),
+	}
+}
+
+func (self *ClassLoader) LoadClass(name string) *Class {
+	if class, ok := self.classMap[name]; ok {
+		// alreay loaded
+		return class
+	}
+	return self.loadNonArrayClass(name)
+}
+
+func (self *ClassLoader) loadNonArrayClass(name string) *Class {
+	data, entry := self.readClass(name)
+	class := self.defineClass(data)
+	link(class)
+	fmt.Printf("[Loaded %s from %s]\n", name, entry)
+	return class
+}
+
+func (self *ClassLoader) readClass(name string) ([]byte, classpath.Entry) {
+	data, entry, err := self.cp.ReadClass(name)
+	if err != nil {
+		panic("java.lang.ClassNotFoundException: " + name)
+	}
+	return data, entry
+}
+
+func (self *ClassLoader) defineClass(data []byte) *Class {
+	class := parseClass(data)
+	class.loader = self
+	resolveSuperClass(class)
+	resolveinterfaces(class)
+	self.classMap[class.name] = class // 注册登记
+	return class
+}
+
+func parseClass(data []byte) *Class {
+	cf, err := classfile.Parse(data)
+	if err != nil {
+		panic(err)
+	}
+	return newClass(cf)
+}
+
+func resolveSuperClass(class *Class) {
+	if class.name != "java/lang/Object" {
+		class.superClass = class.loader.LoadClass(class.superClassName)
+	}
+}
+
+func resolveInterfaces(class *Class) {
+	interfaceCount := len(class.interfaceNames)
+	if interfaceCount > 0 {
+		class.interfaces = make([]*Class, interfaceCount)
+		for i, interfaceName := range class.interfaceNames {
+			class.interfaces[i] = class.loader.LoadClass(interfaceName)
+		}
 	}
 }
